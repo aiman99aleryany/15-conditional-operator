ac83e51c6c827fa77fec0e33b915d99513b840c1
aiman99aleryany <102668388+aiman99aleryany@users.noreply.github.com>
2022-09-16 18:56:10 +0300

    init commit

---
+++ exercise.js
@@ -0,0 +1,6 @@
+// # Exercise 15
+
+// Create a variable called `age` and assign it a numeric value. Use the Conditional (ternary) operator. If the variable is grater or equal to 18, print `You can drive a car`. If the variable is less than 18, print `You are too young to drive`.
+
+let age = 20;
+console.log(age < 18 ? "you are too young to drive" : "You can drive a car");
